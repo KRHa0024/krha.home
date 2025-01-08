1be522852424e888fca6fc1abb2fc7c238f8fb31
KRHa <3dssmg2@gmail.com>
2025-01-09 03:35:32 +0900

    🔥 自動インポート設定を削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-09',
+  compatibilityDate: "2025-01-09",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
-
-module.exports = {
-  components: [
-    {
-      path: '@/components/',
-      pathPrefix: false
-    }
-  ],
-}
+  modules: ["@nuxtjs/tailwindcss"],
+});
