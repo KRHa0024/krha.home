311a425303ee714ec80a8910904ec1e0f1149d25
KRHa <3dssmg2@gmail.com>
2025-01-09 03:03:22 +0900

    ✨ lint入れたつもり

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="flex h-screen items-center justify-center bg-gray-800">
-  <p class="font-bold">
-    Hello Tailwind
-  </p>
-</div>
+    <p class="font-bold">Hello Tailwind</p>
+  </div>
 </template>
