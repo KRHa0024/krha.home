9f6f1f3ad3943debc08d3d6ca4919b38b72c7075
KRHa <3dssmg2@gmail.com>
2025-01-09 02:50:07 +0900

    ✨ 初期設定+tailwind導入

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div class="flex h-screen items-center justify-center bg-gray-800">
+  <p class="font-bold">
+    Hello Tailwind
+  </p>
+</div>
 </template>
